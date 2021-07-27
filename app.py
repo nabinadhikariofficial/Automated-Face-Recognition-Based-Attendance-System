a9b97c7f762deca29fe894c8189b06db7fb8479d
Abhinav0057 <abhinav00aryl57@gmail.com>
2021-07-27 18:54:51 +0545

    User-can-upload-image

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from retinaface import RetinaFace
 from matplotlib import pyplot as plt
 from flask import Flask, request, render_template, jsonify, Markup, session, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'key'
 """ faces = RetinaFace.detect_faces("download.jpg")
 for face in faces:
@@ -15,6 +23,27 @@ port_add = 5000
 @app.route('/')
 def index():
     return render_template('login.html')
+
+
+# webpage where user can provide image
+@app.route('/getimage', methods=['GET', 'POST'])
+def getimage():
+
+    if request.method == 'POST':
+        file = request.files['file']
+        if 'file' not in request.files:
+            message = "Please Select a Image first"
+        elif file.filename == '':
+            message = "Please Select a Image first"
+
+        else:
+            message = "Image accepted"
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        context = {'message': message}
+        return render_template('getimage.html', context=context)
+    else:
+        return render_template('getimage.html', context={})
 
 
 # Running the app
