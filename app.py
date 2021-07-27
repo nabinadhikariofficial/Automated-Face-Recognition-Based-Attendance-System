df8c9d4ae733e9161fbd4a46ef8dae960c609a1d
nabinadhikari70 <nabinadhikari70@gmail.com>
2021-07-27 14:24:48 +0545

    flask setup done

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from retinaface import RetinaFace
 import matplotlib.pyplot as plt
-from flask import Flask, render_template
+from flask import Flask, request, render_template, jsonify, Markup, session, redirect, url_for
 
 app = Flask(__name__)
-faces = RetinaFace.detect_faces("download.jpg")
+app.secret_key = 'key'
+""" faces = RetinaFace.detect_faces("download.jpg")
 for face in faces:
-    print(face)
+    print(face) """
+
+host_add = '127.0.0.1'
+port_add = 5000
 
 
 @app.route('/')
 def index():
-    render_template('login.html')
+    return render_template('login.html')
 
 
+# Running the app
 if __name__ == "__main__":
-    app.run("0.0.0.0", 5000, True)
+    app.run(host=host_add, port=port_add, debug=True)
