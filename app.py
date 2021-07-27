1afba1cb5a4005d6d9bebd45806884fb669f0b3b
nabinadhikari70 <nabindhikari70@gmail.com>
2021-07-27 13:16:15 +0545

    commit check

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ from retinaface import RetinaFace
 import matplotlib.pyplot as plt
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
-face_detector = RetinaFace(quality='high')
 faces = RetinaFace.detect_faces("download.jpg")
 for face in faces:
     print(face)
