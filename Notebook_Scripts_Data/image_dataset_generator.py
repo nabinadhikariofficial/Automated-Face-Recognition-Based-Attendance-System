a822b17d014241552c908eb915715bc07cc1244d
bipe-n <raut.bipen9@gmail.com>
2021-08-28 11:39:25 +0545

    image augumentaion

--- Notebook_Scripts_Data/image_dataset_generator.py
+++ Notebook_Scripts_Data/image_dataset_generator.py
@@ -1,14 +1,25 @@
 from PIL import Image, ImageEnhance
 
-CRN = "KCE074BCT023"
+CRN = "KCE074BCT005"
 image = Image.open("data/"+CRN+".jpg")
 
-enhancer = ImageEnhance.Brightness(image)
-enhancer2 = ImageEnhance.Contrast(image)
+B_enhancer = ImageEnhance.Brightness(image)
+C_enhancer = ImageEnhance.Contrast(image)
+hoz_flip = image.transpose(Image.FLIP_LEFT_RIGHT)
 
-for i in range(8):
-    factor = i/4.0
-    im = enhancer.enhance(factor)
-    im1 = enhancer2.enhance(factor)
-    im.save("data/"+CRN+"_"+"B_"+str(factor)+".jpg")
-    im1.save("data/"+CRN+"_"+"C_"+str(factor)+".jpg")
+
+
+
+for i in range(16):
+    if i>0:    
+        factor = i/8.0
+        im = B_enhancer.enhance(factor)
+        im1 = C_enhancer.enhance(factor)
+        rotate_image=image.rotate(5+i*2)
+       
+        im.save("data/"+CRN+"_"+"B_"+str(factor)+".jpg")
+        im1.save("data/"+CRN+"_"+"C_"+str(factor)+".jpg")
+       
+        rotate_image.save("data/"+CRN+"_"+"R_"+str(5+i*5)+".jpg")
+        
+hoz_flip.save("data/"+CRN+"_"+"hf_"+str(factor)+".jpg")
