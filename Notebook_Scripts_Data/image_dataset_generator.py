63c01d3c129abd056b228ca6054ee3fb64ee07dc
nabinadhikari70 <nabinadhikari70@gmail.com>
2021-08-28 17:42:25 +0545

    data images augmented added

--- Notebook_Scripts_Data/image_dataset_generator.py
+++ Notebook_Scripts_Data/image_dataset_generator.py
@@ -1,24 +1,29 @@
 from PIL import Image, ImageEnhance
 import os
+import pandas as pd
 
-""" CRN = "KCE074BCT005"
-image = Image.open("data/im"+CRN+".jpg")
 
-enhancer = ImageEnhance.Brightness(image)
-enhancer2 = ImageEnhance.Contrast(image)
+def manipulation(image_path):
+    image = Image.open(image_path)
+    B_enhancer = ImageEnhance.Brightness(image)
+    C_enhancer = ImageEnhance.Contrast(image)
+    hoz_flip = image.transpose(Image.FLIP_LEFT_RIGHT)
+    image_loc = image_path[:-4]
+    for i in range(16):
+        if i > 0:
+            factor = i/8.0
+            im = B_enhancer.enhance(factor)
+            im1 = C_enhancer.enhance(factor)
+            rotate_image = image.rotate(5+i*2)
+            im.save(image_loc+"_B_"+str(factor)+".jpg")
+            im1.save(image_loc+"_C_"+str(factor)+".jpg")
+            rotate_image.save(image_loc+"_R_"+str(5+i*5)+".jpg")
+    hoz_flip.save(image_loc+"_hf.jpg")
 
-for i in range(8):
-    factor = i/4.0
-    im = enhancer.enhance(factor)
-    im1 = enhancer2.enhance(factor)
-    im.save("data/"+CRN+"_"+"B_"+str(factor)+".jpg")
-    im1.save("data/"+CRN+"_"+"C_"+str(factor)+".jpg")
-    im
-    im1
- """
-count = 0
-files = os.listdir("data/KCE074BCT005")
-for file in files:
-    count = count+1
-    print(file)
-print(len(files))
+
+data_crn = pd.read_csv("crnAndName.csv")
+for crn in data_crn['CRN']:
+    filepath = "data/"+crn+"/"
+    filenames = os.listdir(filepath)
+    for file in filenames:
+        manipulation(filepath+file)
