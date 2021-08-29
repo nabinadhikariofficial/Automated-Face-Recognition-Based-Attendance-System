022498d21ffa1cb8d0054b8bca1cecb3d2f94807
Samundra13 <samundra1313@gmail.com>
2021-08-29 11:53:17 +0545

    hawa

--- deployment/app.py
+++ deployment/app.py
@@ -67,6 +67,53 @@ def detectfaces():
 @app.route('/takeattendance' , methods=['GET', 'POST'])
 def takeattendance():
     return render_template('takeattendance.html')
+@app.route('/capture' , methods=['GET', 'POST'])
+def capture():
+    
+    # define a video capture object
+    video_capture = cv2.VideoCapture(0)
+
+  
+    while(True):
+        if not video_capture.isOpened():
+            print('Unable to load camera.')
+            sleep(5)
+            pass
+
+
+        ret, image = video_capture.read()
+
+
+
+
+        # Display the resulting image
+        cv2.imshow('Video', image)
+
+        if cv2.waitKey(1) & 0xFF == ord('s'): 
+
+            check, image = video_capture.read()
+            cv2.imshow("Capturing", image)
+            cv2.imwrite(filename='saved_img.jpg', img=image)
+            #video_capture.release()
+            #img_new = cv2.imread('saved_img.jpg')
+            #img_new = cv2.imshow("Captured Image", img_new)
+            #cv2.waitKey(1650)
+            #cv2.destroyAllWindows()
+
+            #break
+        elif cv2.waitKey(1) & 0xFF == ord('a'):
+            video_capture.release()
+            cv2.destroyAllWindows()
+            break
+
+
+        cv2.imshow('Video', image)
+
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+ 
 
 @app.route('/attendancedetails' , methods=['GET', 'POST'])
 def attendancedetails():
