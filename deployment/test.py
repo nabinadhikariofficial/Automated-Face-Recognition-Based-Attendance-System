90a17080857bf91c2773585229b03f62b13e844d
nabinadhikari70 <nabinadhikari70@gmail.com>
2022-02-15 09:26:32 +0545

    model addition

--- deployment/test.py
+++ deployment/test.py
@@ -2,5 +2,5 @@ import pandas as pd
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 maindir = basedir[:-11]
-data = pd.read_csv(maindir+"\\Notebook_Scripts_Data\\studentdetails.csv", index_col=0),T[username].to_dict()
-print(ind.keys())
+print(basedir)
+print(maindir)
