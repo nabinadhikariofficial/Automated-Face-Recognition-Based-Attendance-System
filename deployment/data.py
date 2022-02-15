33f0e7f80e56797122a0b7c24cbf5352c4cf8e4e
Bipen Raut <raut.bipen9@gmail.com>
2022-02-15 11:25:13 +0545

    json database modified

--- deployment/data.py
+++ deployment/data.py
@@ -40,36 +40,44 @@ data={
 
     "attendance":
     {
-        "information system":
+        "information system":[
             {
                 "date": "",
                 "student_username":[]
             },
-        "big data":
+        ],
+
+        "big data":[
             {
                 "date":"",
                 "student_username":[]
             },
-        "Simulation and modelling":
+        ],
+        "Simulation and modelling":[
             {
                 "date":"" ,
                 "student_username":[]
             },
-        "engineering professional practise ":
+        ],
+        "engineering professional practise ":[
             {
                 "date": "",
                 "student_username":[]
             },
-        "internet and intranet":
+        ],
+
+        "internet and intranet":[
             {
                 "date":"" ,
                 "student_username":[]
             },
-        "multimedia system":
+        ],
+        "multimedia system":[
             {
                 "date":"" ,
                 "student_username":[]
             }
+        ]
         
     }
     
@@ -88,12 +96,12 @@ data={
 
 # subject = {"Information System":{
 #     "Present":0,
-#     "absent":0 
+#     "total":0 
 #     }
 # }
 # data["student"]["kce07bct001"]={"Information System":{
 #     "Present":0,
-#     "absent":0
+#     "total":0
 #     }
 # }
 # print(data["teacher"]["SS"]["name"])
@@ -109,27 +117,27 @@ while i < 49:
     
     data["student"][roll_no]={"Information System":{
         "Present":0,
-        "absent":0
+        "total":0
         },
         "Multimedia System":{
         "Present":0,
-        "absent":0
+        "total":0
         },
         "Engineering Professional Practise":{
         "Present":0,
-        "absent":0
+        "total":0
         },
         "Big Data ":{
         "Present":0,
-        "absent":0
+        "total":0
         },
         "Internet and Intranet":{
         "Present":0,
-        "absent":0
+        "total":0
         },
         "Simulation and Modelling":{
         "Present":0,
-        "absent":0
+        "total":0
         },
         
         
